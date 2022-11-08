683c55018eb693d3e7b177ca0e49e1e0cd218d12
刘伟 <david.lauwei@hotmail.com>
2022-11-08 13:44:43 +0800

    Delete 1.3.0/Examples/uartDMASendReceive/MDK/Objects directory

--- 1.3.0/Examples/uartDMASendReceive/MDK/Objects/system_cms32l051.d
+++
@@ -1,8 +0,0 @@
-.\objects\system_cms32l051.o: RTE/Device/CMS32L051LQ48/system_CMS32L051.c
-.\objects\system_cms32l051.o: C:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_cms32l051.o: C:\Program Files\Keil\Packs\Cmsemicon\CMS32L051\1.3.0\Device\Include\CMS32L051.h
-.\objects\system_cms32l051.o: C:\Program Files\Keil\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\system_cms32l051.o: C:\Program Files\Keil\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\system_cms32l051.o: C:\Program Files\Keil\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\system_cms32l051.o: C:\Program Files\Keil\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\system_cms32l051.o: C:\Program Files\Keil\Packs\Cmsemicon\CMS32L051\1.3.0\Device\Include\system_CMS32L051.h
